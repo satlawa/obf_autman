a5ae87d6ec7b2212ac098c0313f480ab4935be61
satlawa <satlawa@protonmail.com>
2021-11-18 22:26:13 +0100

    added value ranges

---
+++ DataStructure.py
@@ -0,0 +1,35 @@
+
+# wo
+self.check_u = [80, 100, 120, 140, 160, 200]
+
+self.check_w = ['WI', 'WA', 'SSI', 'SSA', 'OSI', 'OSA', 'BSI', 'BSA']
+
+self.chk_stoe = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 21, 22, 23,\
+ 25, 26, 27, 28, 31, 32, 33, 34, 35, 36, 37, 41, 42, 43, 44, 51, 52, 53, 54,\
+ 55, 56, 57, 58, 61, 62, 63, 64, 65, 66, 71, 72, 73, 74, 75, 76, 81, 82, 83,\
+ 84, 85, 86, 86, 87, 88, 89, 91, 92, 93, 94, 95, 96, 97, 98]
+
+self.chk_vtyp = []
+
+self.chk_vg = [0, 1, 2, 3, 4, 5]
+self.chk_swe = ['grün', 'gelb', 'rot', 'Grün', 'Gelb', 'Rot']
+
+# bz
+self.chk_ba = ["AS", "AZ", "AH", "PM", "BI", "FB", "BL", "AC", "DG", "EE",\
+ "EK", "EB", "EI", "EL", "ES", "EA", "FA", "FI", "FE", "FZ", "GK", "GB", "WP",\
+ "GE", "AG", "AV", "HB", "HT", "HP", "JL", "CJ", "KK", "KB", "KO", "LA", "LI",\
+ "ME", "AN", "FO", "PO", "AB", "RO", "RK", "BU", "RE", "SW", "CH", "ER", "SK",\
+ "JN", "SP", "SF", "LS", "SL", "SN", "SG", "PU", "SA", "QR", "ST", "KW", "TH",\
+ "QP", "TK", "TB", "UL", "NU", "WD", "KI", "TA", "WO", "LW", "EZ", "ZI", "SL",\
+ "SN"]
+
+# nutz
+self.chk_nutz_ma = ['AD', 'AE', 'AF', 'AG', 'AS', 'BU', 'DE', 'DF', 'DP',\
+ 'EG', 'FM', 'JF', 'JP', 'KE', 'KF', 'KH', 'LI', 'LL', 'NB', 'ND':, 'PA',\
+ 'PL', 'RM', 'SB', 'TR', 'UE', 'ZE', 'ZN', 'ZV']
+
+self.chk_nutz_dring = [1, 2, 3, 4, 5, 6]
+self.chk_nutz_bewil = [1, 2, 3]
+self.chk_nutz_zeitp = [1, 2, 3]
+self.chk_nutz_schlg = [1, 2, 3, 4, 5, 6]
+self.chk_nutz_rueck = [0, 10, 23, 26, 29, 30, 31, 35, 36, 38, 40, 90]
